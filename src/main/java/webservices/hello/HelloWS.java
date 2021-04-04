809dfd7fa945b727233e2d9f61595b398e84542f
bookgvi <bookgvi@gmail.com>
2021-04-04 13:01:25 +0300

    явное определение стиля и тела для SOAP

--- src/main/java/webservices/hello/HelloWS.java
+++ src/main/java/webservices/hello/HelloWS.java
@@ -6,7 +6,10 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
 @WebService
-@SOAPBinding(style = SOAPBinding.Style.RPC)
+@SOAPBinding(
+        style = SOAPBinding.Style.DOCUMENT,
+        use = SOAPBinding.Use.LITERAL
+)
 public interface HelloWS {
 
     @WebMethod
